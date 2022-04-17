8a640133ed0f6e5b42cd8af53e63f741711359f2
Bastien Traille <bastien.traille@gmail.com>
2022-04-17 19:47:32 +0200

    first commit with homepage bg and title

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import affiche from './assets/img/affiche.jpg';
+
+function App() {
+  return (
+    <body className="body">
+      <div className="landing">
+        <img src={affiche} alt="" />
+        <h1>La Marcel Queheille CycloSportive</h1>
+      </div>
+    </body>
+  );
+}
+
+export default App;
